34cb228843bbdda8bac8857ebde15bd9c8571daa
Adarsh Nanwani <adarsh.nanwani@gmail.com>
2019-11-11 11:43:43 +0530

    Added UUID key

--- src/ShoppingList.js
+++ src/ShoppingList.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import ShoppingListForm from "./ShoppingListForm";
+import uuid from "uuid/v4";
 
 class ShoppingList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      list: [{ name: "1", quantity: "sfd" }, { name: "2", quantity: "ytu" }]
+      list: [
+        { name: "1", quantity: "sfd", id: uuid() },
+        { name: "2", quantity: "ytu", id: uuid() }
+      ]
     };
     this.renderItems = this.renderItems.bind(this);
     this.addItem = this.addItem.bind(this);
@@ -13,16 +17,17 @@ class ShoppingList extends Component {
 
   renderItems() {
     return this.state.list.map(item => (
-      <li>
+      <li key={item.id}>
         {item.name}:{item.quantity}
       </li>
     ));
   }
 
   addItem(item) {
+    let newItem = { ...item, id: uuid() };
     this.setState(state => {
       return {
-        list: [...state.list, item]
+        list: [...state.list, newItem]
       };
     });
   }
@@ -32,7 +37,7 @@ class ShoppingList extends Component {
       <div>
         <h2>Shopping List</h2>
         <ul>{this.renderItems()}</ul>
-        <ShoppingListForm addItem={this.addItem}/>
+        <ShoppingListForm addItem={this.addItem} />
       </div>
     );
   }
